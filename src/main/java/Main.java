3d9321a2eacd1cb6050b28e5eb20bfee522d6eac
TobiasKrok <ekoncraft@hotmail.no>
2024-09-08 12:34:11 +0200

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,18 @@
 import core.Redis;
 
+import java.util.Arrays;
+
 public class Main {
   public static void main(String[] args){
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
 
+
       int port = 6379;
+      // only --port is supported for now
+      if(args.length == 2) {
+        port = Integer.parseInt(args[1]);
+      }
       Redis redis = new Redis(port);
       redis.startServer();
   }
