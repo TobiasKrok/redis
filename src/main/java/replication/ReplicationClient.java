95ad49b4f4a6748e287c020ce188a8e5e31db95d
TobiasKrok <ekoncraft@hotmail.no>
2024-09-17 21:34:28 +0200

    codecrafters submit [skip ci]

--- src/main/java/replication/ReplicationClient.java
+++ src/main/java/replication/ReplicationClient.java
@@ -13,11 +13,10 @@ public class ReplicationClient implements Runnable {
     private final int port;
     private final String masterHost;
     private final int masterPort;
-    private volatile boolean handShakeCompleted = false;
-
-    private SocketChannel channel;
-
     private final Object lock = new Object();
+    private volatile boolean handShakeCompleted = false;
+    private volatile boolean handShakeInProgress = false;
+    private SocketChannel channel;
 
     public ReplicationClient(int port, String masterHost, int masterPort) {
         this.port = port;
@@ -26,15 +25,15 @@ public class ReplicationClient implements Runnable {
     }
 
     @Override
-    public void run()  {
+    public void run() {
         try {
-            System.out.println("running!");
-            if(!handShakeCompleted) {
-                synchronized (lock) {
-                    if(channel == null) {
-                        channel = SocketChannel.open();
-                        channel.connect(new InetSocketAddress(masterHost, masterPort));
-                    }
+            synchronized (lock) {
+                if (channel == null) {
+                    channel = SocketChannel.open();
+                    channel.connect(new InetSocketAddress(masterHost, masterPort));
+                }
+                if (!handShakeCompleted && !handShakeInProgress) {
+                    handShakeInProgress = true;
                     initiateHandShake(channel);
                 }
             }
@@ -43,27 +42,37 @@ public class ReplicationClient implements Runnable {
         }
 
     }
-    public void startReplicationServer() {
-        try(SocketChannel channel = SocketChannel.open()) {
-            channel.connect(new InetSocketAddress(masterHost, masterPort));
-            initiateHandShake(channel);
-        } catch (IOException e) {
-            System.err.println(e.getMessage());
-        }
-    }
 
     private void initiateHandShake(SocketChannel channel) throws IOException {
         System.out.println("Initiating handshake...");
         channel.write(ByteBuffer.wrap(RespParser.fromArray(List.of("PING"))));
         System.out.println("Sent PING to master");
         ByteBuffer buffer = ByteBuffer.allocate(256);
-        channel.read(buffer); // blocking
-        buffer.flip();
-        String res = RespParser.read(buffer).getFirst();
-        if(!res.equalsIgnoreCase("pong")) throw new RuntimeException("Couldn't contact master");
+        String res = read(channel, buffer).getFirst();
+        if (!res.equalsIgnoreCase("pong")) throw new RuntimeException("Couldn't contact master");
 
-
+        // replconf
         channel.write(ByteBuffer.wrap(RespParser.fromArray(List.of("replconf", "listening-port", String.valueOf(port)))));
+        if(!read(channel, buffer).getFirst().equalsIgnoreCase("OK"))
+            throw new RuntimeException("redis: repl master did not accept our port binding");
+
         channel.write(ByteBuffer.wrap(RespParser.fromArray(List.of("replconf", "capa", "psync2"))));
+        if(!read(channel, buffer).getFirst().equalsIgnoreCase("OK"))
+            throw new RuntimeException("redis: repl master did not accept our capa sync");
+
+
+            // psync - ? is the replication ID which we do not know yet, and the default offset is -1 when connecting for the first time.
+        channel.write(ByteBuffer.wrap(RespParser.fromArray(List.of("PSYNC", "?", "-1"))));
+
+        handShakeCompleted = true;
+
+    }
+
+    private List<String> read(SocketChannel channel, ByteBuffer buffer) throws IOException {
+        buffer.clear();
+        channel.read(buffer);
+        buffer.flip();
+
+        return RespParser.read(buffer);
     }
 }
